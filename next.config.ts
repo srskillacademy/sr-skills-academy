d4b94fb2abb6cab812d833a8200c528698314191
Rushikesh Kulkarni <rushikeshkulkarni06@gmail.com>
2026-08-29 23:46:21 +0530

    Configure Next.js static export for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  trailingSlash: true,
+
+  basePath: "/sr-skills-academy",
+  assetPrefix: "/sr-skills-academy/",
 };
 
 export default nextConfig;
